a804e841fc686875a6a003cf46c703bcf4e7eba1
Arkady Zelensky <arkasha9606@gmail.com>
2017-11-07 21:57:31 +0200

    adding js for PWA0

--- sw.js
+++ sw.js
@@ -1,6 +1,32 @@
-/**
- * Created by ARK on 07.11.2017.
- */
-
-'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(["index.html","style/style.css"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+self.addEventListener('install', e => {
+  e.waitUntil(
+    // после установки service worker
+    // открыть новый кэш
+    caches.open('my-pwa-cache').then(cache => {
+      // добавляем все URL ресурсов, которые хотим закэшировать
+      return cache.addAll([
+        '/',
+        '/profile',
+        '/about',
+        '/assets/images/logo.png',
+        '/assets/images/lapkaGotovaya.svg',
+        '/assets/images/background0.jpg',
+        '/styles/css/main.css',
+        '/styles/css/base.css',
+        '/styles/css/reset.css',
+        '/styles/css/preloaders.css',
+        '/styles/css/error.css',
+        '/styles/css/header.css',
+        '/styles/css/about.css',
+        '/styles/css/issues.css',
+        '/styles/css/profile.css',
+        '/styles/css/singleIssue.css',
+        '/scripts/index.js',
+        '/scripts/about.js',
+        '/scripts/issues.js',
+        '/scripts/preloaders.js',
+        '/scripts/singleIssue.js'
+      ]);
+    })
+  );
+});
